354687ea9a4d249b50aaeca274e3a6d5a70d362f
Ikram Awol <99601469+ikramawol@users.noreply.github.com>
2023-06-15 19:21:52 +0300

    Add files via upload

---
+++ simple_calculator.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float i,j,sum,difference,product,quotient;
+	cout<<"enter two numbers\n";
+	cin>>i>>j;
+	sum=i+j;
+	cout<<"sum: "<<sum<<endl;
+	difference=i-j;
+	cout<<"difference: "<<difference<<endl;
+	product=i*j;
+	cout<<"product: "<<product<<endl;
+	quotient=i/j;
+	cout<<"quotient: "<<quotient<<endl;
+	
+	return 0;
+}
